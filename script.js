2481af4ca2a9fbdbf139c960093080685a51ea0f
ferreirasilvathiago1604-source <ferreira.silva.thiago1604@escola.pr.gov.br>
2026-06-11 08:03:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.innerHTML =
+    "O agronegócio e a agropecuária representam inovação, sustentabilidade e desenvolvimento. O campo produz alimentos, gera empregos e contribui para um futuro melhor para todos.";
+}
